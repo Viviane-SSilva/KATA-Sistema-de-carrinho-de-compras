ce415f96634b23a9f09babce1694c14bec1311c3
Viviane Silva <viviane.silva1810@gmail.com>
2025-09-27 14:47:15 -0300

    chore: arquivo que coordena as regras de negocio.

---
+++ src/servicos/CarrinhoService.js
@@ -0,0 +1,35 @@
+import ProdutoValidator from "../validacao/ProdutoValidator.js";
+import CarrinhoRepo from "../repositorios/CarrinhoRepo.js";
+
+class CarrinhoService {
+  constructor() {
+    this.repositorio = new CarrinhoRepo();
+  }
+
+  adicionarProduto(produto) {
+    const validado = ProdutoValidator.validar(produto);
+    this.repositorio.adicionarProduto(validado.nome, {
+      preco: validado.preco,
+      quantidade: validado.quantidade,
+    });
+    return this.repositorio.listar().find((p) => p.nome === validado.nome);
+  }
+
+  removerProduto(nome) {
+    return this.repositorio.remover(nome);
+  }
+
+  alterarQuantidade(nome, quantidade) {
+    return (this, this.repositorio.alterarQuantidade(nome, quantidade));
+  }
+
+  listarProdutos() {
+    return this.repositorio.listar();
+  }
+
+  calcularTotal() {
+    return this.repositorio.calcularTotal();
+  }
+}
+
+export default CarrinhoService;
