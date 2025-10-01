ce1a15a99b83b5b9d07aca79b257cbe897c4c82c
Viviane Silva <viviane.silva1810@gmail.com>
2025-09-30 23:53:53 -0300

    chore:  implementacao de controller Alterar produto.

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { AdicionarProdutoController } from '../controllers/AdicionarProduto.controller';
+import { AlterarQuantidadeController } from '../controllers/AlterarQuantidadeProd.controller';
 
 const routes = Router();
 
 routes.post('/criar-produto', AdicionarProdutoController);
+routes.post('/alterar-quantidade-produto', AlterarQuantidadeController);
 
 export { routes };
