9cebabe17eaa1167b79a1b0d68113f58926673fd
Viviane Silva <viviane.silva1810@gmail.com>
2025-10-01 00:51:32 -0300

    chore: ajuste de modo de importação dos controllers

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { AdicionarProdutoController } from '../controllers/AdicionarProduto.controller';
-import { AlterarQuantidadeController } from '../controllers/AlterarQuantidadeProd.controller';
-import { ListarProdutoController } from '../controllers/ListarProduto.controller';
-import { RemoverProdutoController } from '../controllers/RemoverProduto.controller';
+import { AdicionarProdutoController } from '../controllers/AdicionarProduto.controller.js';
+import { AlterarQuantidadeController } from '../controllers/AlterarQuantidadeProd.controller.js';
+import { ListarProdutoController } from '../controllers/ListarProduto.controller.js';
+import { RemoverProdutoController } from '../controllers/RemoverProduto.controller.js';
 
 const routes = Router();
 
