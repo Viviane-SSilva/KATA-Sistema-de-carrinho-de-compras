b3812f6da4d258232eab1229b708c13117788f1c
Viviane Silva <viviane.silva1810@gmail.com>
2025-09-30 23:58:04 -0300

    chore: implementacao de controller listar produto.

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { AdicionarProdutoController } from '../controllers/AdicionarProduto.controller';
 import { AlterarQuantidadeController } from '../controllers/AlterarQuantidadeProd.controller';
+import { ListarProdutoController } from '../controllers/ListarProduto.controller';
 
 const routes = Router();
 
 routes.post('/criar-produto', AdicionarProdutoController);
 routes.post('/alterar-quantidade-produto', AlterarQuantidadeController);
+routes.get('/listar-produtos', ListarProdutoController);
 
 export { routes };
